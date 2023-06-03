544249867fc9efc0db4136d6e915f3b3da4f3c49
Bethany Grigsby <grigsby.bethany@gmail.com>
2023-06-03 10:03:47 -0700

    fixed formatting issues

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -17,19 +17,19 @@ function invalidNumber(num) {
 
 prompt('Welcome to the Calculator');
 
-prompt('What is the first number?')
+prompt('What is the first number?');
 let number1 = readline.question();
 
 while (invalidNumber(number1)) {
-  prompt('Hmmm...that doesn\'t look like a valid number.')
+  prompt('Hmmm...that doesn\'t look like a valid number.');
   number1 = readline.question();
 }
 
-prompt('What is the second number?')
+prompt('What is the second number?');
 let number2 = readline.question();
 
 while (invalidNumber(number2)) {
-  prompt('Hmmm...that doesn\'t look like a valid number.')
+  prompt('Hmmm...that doesn\'t look like a valid number.');
   number2 = readline.question();
 }
 
@@ -38,7 +38,7 @@ prompt(`What operation would you like to peform?
 let operation = readline.question();
 
 while (!['1' , '2', '3', '4'].includes(operation)) {
-  prompt('Must choose 1, 2, 3, or 4.')
+  prompt('Must choose 1, 2, 3, or 4.');
   operation = readline.question();
 }
 
@@ -49,13 +49,13 @@ switch (operation) {
     break;
   case '2':
     output = Number(number1) - Number(number2);
-    break; 
-  case '3': 
+    break;
+  case '3':
     output = Number(number1) * Number(number2);
     break;
   case '4':
     output = Number(number1) / Number(number2);
-    break; 
+    break;
 }
 
 prompt(`The result is ${output}`);
